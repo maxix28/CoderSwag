3e90885d317748ec38e31069a00596833d549522
MaxPavliv <maxyxpavliv@gmail.com>
2023-08-27 15:44:36 +0300

    add layout for product item

--- app/src/main/java/com/example/coderswag/serices/DataService.kt
+++ app/src/main/java/com/example/coderswag/serices/DataService.kt
@@ -17,25 +17,35 @@ object DataService  {
         Category("DIGITAL","digitalgoodsimage")
     )
 val hats = listOf(
-    Product("Devslopes Graphic Beanie", "$18","hat01"),
-    Product("Devslopes Hat Black", "$20","hat02"),
-    Product("Devslopes Hat White", "$18","hat03"),
-    Product("Devslopes Hat Snapback", "$22","hat04")
+    Product("Devslopes Graphic Beanie", "$18","hat1"),
+    Product("Devslopes Hat Black", "$20","hat2"),
+    Product("Devslopes Hat White", "$18","hat3"),
+    Product("Devslopes Hat Snapback", "$22","hat4")
 )
 val hoodies= listOf(
-    Product("Devslopes Hoodie Gray", "$28","hoodie01"),
-    Product("Devslopes Hoodie Red", "$32","hoodie02"),
-    Product("Devslopes Gray Hoodie", "$28","hoodie03"),
-    Product("Devslopes Black Hoodie", "$32","hoodie04"),
+    Product("Devslopes Hoodie Gray", "$28","hoodie1"),
+    Product("Devslopes Hoodie Red", "$32","hoodie2"),
+    Product("Devslopes Gray Hoodie", "$28","hoodie3"),
+    Product("Devslopes Black Hoodie", "$32","hoodie4"),
     )
 
     val shirt= listOf(
-        Product("Devslopes Shirt Black ", "$28","shirt01"),
-        Product("Devslopes Badge Light Gray ", "$20","shirt02"),
-        Product("Devslopes Logo Shirt Red ", "$20","shirt03"),
-        Product("Devslopes Hustle ", "$22","shirt04"),
-        Product("Kickflip Studio", "$18","shirt05"),
+        Product("Devslopes Shirt Black ", "$28","shirt1"),
+        Product("Devslopes Badge Light Gray ", "$20","shirt0"),
+        Product("Devslopes Logo Shirt Red ", "$20","shirt3"),
+        Product("Devslopes Hustle ", "$22","shirt4"),
+        Product("Kickflip Studio", "$18","shirt5"),
 
 
         )
+
+    val  digitalGood= listOf<Product>()
+    fun getProducts(category: String) : List<Product>{
+        return when(category){
+            "SHIRT"-> shirt
+            "HATS"-> hats
+            "HOODIES"-> hoodies
+            else -> digitalGood
+        }
+    }
 }
